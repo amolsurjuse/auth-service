4b1df3e13e3152046586df35e62b8d0c6b8e5338
Amol Surjuse <amol.surjuse@gmail.com>
2026-01-27 19:51:20 -0500

    Fixed redis connectivity issue

--- src/main/java/com/amy/auth/config/RedisConfig.java
+++ src/main/java/com/amy/auth/config/RedisConfig.java
@@ -2,7 +2,9 @@ package com.amy.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -10,8 +12,16 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public LettuceConnectionFactory redisConnectionFactory(Environment env) {
+        String host = env.getProperty("spring.data.redis.host", "redis");
+        int port = Integer.parseInt(env.getProperty("spring.data.redis.port", "6379"));
+        String password = env.getProperty("spring.data.redis.password");
+
+        RedisStandaloneConfiguration cfg = new RedisStandaloneConfiguration(host, port);
+        if (password != null && !password.isBlank()) {
+            cfg.setPassword(password);
+        }
+        return new LettuceConnectionFactory(cfg);
     }
 
     @Bean
